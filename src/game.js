0b4301723edfda18a8950feddefea8d149d49695
Alex Thompson <ahthomps1@gmail.com>
2021-01-13 16:46:14 -0700

    Create player class framework

---
+++ src/game.js
@@ -0,0 +1,8 @@
+class Game {
+  constructor() {
+
+    this.player1 = new Player('player1');
+    this.player2 = new Player('player2');
+    this.mainDeck = []
+  }
+}
---
+++ src/player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor(id) {
+    this.wins = 0;
+    this.hand = [];
+    this.id = id;
+    this.turn = false;
+  }
+
+  playCard() {
+
+  }
+
+  saveWinsToStorage() {
+    
+  }
+}
