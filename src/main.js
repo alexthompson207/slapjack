f436cfe76ed398fdccf4a124536827e0a0dd9830
Alex Thompson <ahthomps1@gmail.com>
2021-01-15 08:26:18 -0700

    Add dom elements to main.js file, 3 decks and game message

---
+++ src/main.js
@@ -0,0 +1,6 @@
+var game;
+
+var playerOneDeck = document.querySelector('.player-one-deck');
+var playerTwoDeck = document.querySelector('.player-two-deck');
+var centerDeck = document.querySelector('.center-deck');
+var gameMessage = document.querySelector('.game-update');
