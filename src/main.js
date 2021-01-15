9a2c542456b708ea2894350cdc179a25d1df86af
Alex Thompson <ahthomps1@gmail.com>
2021-01-15 09:03:25 -0700

    Create funtion to display center deck on the DOM, needs to become dynamic

--- src/main.js
+++ src/main.js
@@ -4,3 +4,15 @@ var playerOneDeck = document.querySelector('.player-one-deck');
 var playerTwoDeck = document.querySelector('.player-two-deck');
 var centerDeck = document.querySelector('.center-deck');
 var gameMessage = document.querySelector('.game-update');
+
+
+
+function displayCenterPile() {
+  resetCenterPile();
+  var topCard = `<img src="./assets/blue-01.png" alt="Current Played Card" class="current-card cards">`;
+  centerDeck.insertAdjacentHTML('afterbegin', topCard);
+}
+
+function resetCenterPile() {
+  centerDeck.innerHTML = '';
+}
